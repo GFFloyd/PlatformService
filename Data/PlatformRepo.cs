272a682649a3a1bd12e987610fc0181d5074a9ff
GFFloyd <gfaracovip@gmail.com>
2025-04-02 15:11:30 -0300

    - Adicionado interface PlatformRepo - Adicionada classe PlatformRepo que implementa a interface, assim como o corpo dos métodos

---
+++ Data/PlatformRepo.cs
@@ -0,0 +1,21 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+	public class PlatformRepo(AppDbContext context) : IPlatformRepo
+	{
+		private readonly AppDbContext _context = context;
+
+		public void CreatePlatform(Platform platform)
+		{
+			ArgumentNullException.ThrowIfNull(platform);
+			_context.Add(platform);
+		}
+
+		public IEnumerable<Platform> GetAllPlatforms() => [.. _context.Platforms];
+
+		public Platform GetPlatformById(int id) => _context.Platforms.FirstOrDefault(e => e.Id == id);
+
+		public bool SaveChanges() => (_context.SaveChanges() >= 0);
+	}
+}
